fe19130b0f37184163057b471c6152f84621f8ad
Wellington Moraes <wellpunk@gmail.com>
2022-05-08 19:48:20 -0300

    novos arquivos

--- hello.py
+++ hello.py
@@ -8,6 +8,8 @@ Como usar:
 
 Tenha a variável LANG devidamente configurada ex:
 
+    export LANG=pt_BR
+
 Execução:
 
     python3 hello.py
@@ -21,15 +23,13 @@ __license__ = "Unlicense"
 import os
 
 current_language = os.getenv("LANG", "en_US")[:5]
-msg = "Hello, World!"
 
-if current_language == "pt_BR":
-    msg = "Olá, Mundo!"
-elif current_language == "it_IT":
-    msg = "Ciao, Mondo!"
-elif current_language == "es_SP":
-    msg = "Hola, Mundo!"
-elif current_language == "fr_FR":
-    msg = "Bonjour Monde!"
+msg = {
+    "en_US": "Hello, World!",
+    "pt_BR": "Olá, Mundo!",
+    "it_IT": "Ciao, Mondo!",
+    "es_SP": "Hola, Mundo!",
+    "fr_FR": "Bonjour, Monde!"
+}
 
-print(msg)
+print(msg[current_language])
